001f938122c89b7133c2fedf64344c99d5f7e978
Asoler19 <asoler@techlaunch.io>
2018-11-04 01:06:42 -0500

    Updates

---
+++ precache-manifest.2aea23bdee4b8dd5ce5af44c2bdc3436.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff1178aedf2c9234f66f",
+    "url": "/static/css/main.01058008.chunk.css"
+  },
+  {
+    "revision": "ff1178aedf2c9234f66f",
+    "url": "/static/js/main.ff1178ae.chunk.js"
+  },
+  {
+    "revision": "e632832b353e1a4ea043",
+    "url": "/static/js/1.e632832b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6fe4a82186818cadfd4405fcf8bd598d",
+    "url": "/index.html"
+  }
+];
